02c05fa16ceb2f445be768c3e443d7da62b7b39f
danieldantasdev <contatolobinhodev@gmail.com>
2023-07-15 11:47:18 -0300

    feat: implements functionality delete

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -14,6 +14,9 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Task> _tasks = [];
+  late Map<String, dynamic> _lastRemoved;
+  late int _lastRemovedIndex;
+
   final TextEditingController _taskController = TextEditingController();
   final TaskService _taskService = TaskService();
 
@@ -29,6 +32,65 @@ class _HomePageState extends State<HomePage> {
 
       _taskService.create(_tasks);
     });
+  }
+
+  Widget _buildItem(BuildContext context, int index) {
+    Task task = _tasks[index];
+
+    return Dismissible(
+      key: Key(DateTime.now().microsecond.toString()),
+      background: Container(
+        color: Colors.red,
+        child: const Align(
+          alignment: Alignment(0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (dismissDirection) {
+        setState(() {
+          _lastRemoved = Map.from(task.toJson());
+          _lastRemovedIndex = index;
+          _tasks.removeAt(index);
+
+          _taskService.create(_tasks);
+
+          final SnackBar snackBar = SnackBar(
+            content: Text("Tarefa (${_lastRemoved["title"]}) removida!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _tasks.insert(_lastRemovedIndex, _lastRemoved as Task);
+
+                  _taskService.create(_tasks);
+                });
+              },
+            ),
+            duration: const Duration(
+              seconds: 3,
+            ),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        });
+      },
+      child: CheckboxListTile(
+        title: Text(task.title),
+        value: task.ok,
+        onChanged: (bool? value) {
+          setState(() {
+            task.ok = value!;
+            _taskService.create(_tasks);
+          });
+        },
+        secondary: CircleAvatar(
+          child: Icon(task.ok ? Icons.check : Icons.error),
+        ),
+      ),
+    );
   }
 
   @override
@@ -83,22 +145,7 @@ class _HomePageState extends State<HomePage> {
             ),
             Expanded(
               child: ListView.builder(
-                itemBuilder: (context, index) {
-                  Task task = _tasks[index];
-                  return CheckboxListTile(
-                    title: Text(task.title),
-                    value: task.ok,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        task.ok = value!;
-                        _taskService.create(_tasks);
-                      });
-                    },
-                    secondary: CircleAvatar(
-                      child: Icon(task.ok ? Icons.check : Icons.error),
-                    ),
-                  );
-                },
+                itemBuilder: _buildItem,
                 itemCount: _tasks.length,
               ),
             )
